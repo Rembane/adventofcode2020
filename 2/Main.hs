523590a9e6265644575c2370de1e552abf7e7738
Andreas Ekeroot <andeke@gmail.com>
2020-12-05 17:36:45 +0100

    Solve 2.2

--- 2/Main.hs
+++ 2/Main.hs
@@ -24,10 +24,23 @@ parsePasswordPolicy = evalState
   )
 
 -- 1-3 a: abcde
-isValid :: PasswordPolicy -> Bool
-isValid (PP r c p) = inRange r (length (filter (== c) p))
+isValid1 :: PasswordPolicy -> Bool
+isValid1 (PP r c p) = inRange r (length (filter (== c) p))
+
+isValid2 :: PasswordPolicy -> Bool
+isValid2 (PP (p1, p2) c pass) =
+  (== 1)
+    . length
+    . filter
+        (uncurry (&&) . ((uncurry (||) . ((p1 ==) &&& (p2 ==))) *** (== c)))
+    . zip [1 ..]
+    $ pass
 
 main :: IO ()
 main =
   readFile "input.txt"
-    >>= (print . length . filter isValid . map parsePasswordPolicy . lines)
+    >>= ( print
+        . (length . filter isValid1 &&& length . filter isValid2)
+        . map parsePasswordPolicy
+        . lines
+        )
