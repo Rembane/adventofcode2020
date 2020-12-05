f99db164d5869fad6e93d9528c13bd723e564e9e
Andreas Ekeroot <andeke@gmail.com>
2020-12-05 17:12:45 +0100

    Solve 2.1

--- 2/Main.hs
+++ 2/Main.hs
@@ -1,23 +1,33 @@
 module Main where
 
+import           Control.Arrow
+import           Control.Monad
+import           Control.Monad.State.Strict
 import           Data.Char
 import           Data.Ix
 import           Data.List
-import           Debug.Trace
 
-isValid :: String -> Bool
-isValid s =
-  let (lower, '-' : s'                         ) = parseNumber "" s
-      (upper, ' ' : char : ':' : ' ' : password) = parseNumber "" s'
-  in  case find ((== char) . head) $ group $ sort password of
-        Just cs ->
-          let r = inRange (lower, upper) $ length cs
-          in  if r then r else traceShow (r, length cs, s) r
-        _ -> False
- where
-  parseNumber :: String -> String -> (Int, String)
-  parseNumber r (c : cs) | isDigit c = parseNumber (c : r) cs
-                         | otherwise = (read r, c : cs)
+data PasswordPolicy = PP (Int, Int) Char String
+
+mparse :: (String -> a) -> State String a
+mparse f = state ((f *** dropWhile (== ' ')) . span (/= ' '))
+
+mread :: Read a => State String a
+mread = mparse read
+
+parsePasswordPolicy :: String -> PasswordPolicy
+parsePasswordPolicy = evalState
+  (   PP
+  <$> mparse ((read *** read . tail) . span (/= '-'))
+  <*> mparse head
+  <*> mparse id
+  )
+
+-- 1-3 a: abcde
+isValid :: PasswordPolicy -> Bool
+isValid (PP r c p) = inRange r (length (filter (== c) p))
 
 main :: IO ()
-main = readFile "input.txt" >>= (print . length . filter isValid . lines)
+main =
+  readFile "input.txt"
+    >>= (print . length . filter isValid . map parsePasswordPolicy . lines)
